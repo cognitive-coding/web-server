a974056cf72f1b0184027d9db7e0f470c921054d
Curts Hayes <cognitive-coding@protonmail.com>
2017-12-07 13:13:57 -0600

    Refactor middleware

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
+const middleware = require('./middleware');
 const express = require('express');
 const app = express();
 
 var PORT = 3000;
-var middleware = {
-    requireAuthentication: (req, res, next) => {
-        console.log('private route hit!');
-        next();
-    },
-    logger: (req, res, next) => {
-        console.log('Request: ' + new Date().toString() + ' ' +req.method + ' ' + req.originalUrl);
-        next();
-    }
-};
 
 app.use(middleware.logger);
 
